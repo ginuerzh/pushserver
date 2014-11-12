02a93f06821054cbf4cd857cc76c545e89304169
jy.wang <shevilangle@gmail.com>
2014-11-12 13:45:07 +0800

    first commit

---
+++ models/models.go
@@ -0,0 +1,132 @@
+package models
+
+import (
+	"github.com/ginuerzh/sports/errors"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"log"
+)
+
+var (
+	databaseName = "sports"
+	accountColl  = "accounts"
+	MongoAddr    = "localhost:27017"
+	mgoSession   *mgo.Session
+)
+
+type Location struct {
+	Lat float64 `bson:"latitude" json:"latitude"`
+	Lng float64 `bson:"longitude" json:"longitude"`
+}
+
+type Account struct {
+	Id  string    `bson:"_id,omitempty" json:"-"`
+	Loc *Location `bson:",omitempty" json:"-"`
+}
+
+func (this *Account) findOne(query interface{}) (bool, error) {
+	var users []Account
+
+	err := search(accountColl, query, nil, 0, 1, nil, nil, &users)
+	if err != nil {
+		return false, errors.NewError(errors.DbError, err.Error())
+	}
+	if len(users) > 0 {
+		*this = users[0]
+	}
+	return len(users) > 0, nil
+}
+
+func (this *Account) FindByUserid(userid string) (bool, error) {
+	return this.findOne(bson.M{"_id": userid})
+}
+
+func GetListByQuery(query bson.M) (total int, users []Account, err error) {
+	if err := search(accountColl, query, nil, 0, 0, nil, nil, &users); err != nil {
+		return 0, nil, errors.NewError(errors.DbError, err.Error())
+	}
+	return
+}
+
+func search(collection string, query interface{}, selector interface{},
+	skip, limit int, sortFields []string, total *int, result interface{}) error {
+
+	q := func(c *mgo.Collection) error {
+		qy := c.Find(query)
+		var err error
+
+		if selector != nil {
+			qy = qy.Select(selector)
+		}
+
+		if total != nil {
+			if *total, err = qy.Count(); err != nil {
+				return err
+			}
+		}
+
+		if result == nil {
+			return err
+		}
+
+		if limit > 0 {
+			qy = qy.Limit(limit)
+		}
+		if skip > 0 {
+			qy = qy.Skip(skip)
+		}
+		if len(sortFields) > 0 {
+			qy = qy.Sort(sortFields...)
+		}
+
+		return qy.All(result)
+	}
+
+	if err := withCollection(collection, nil, q); err != nil {
+		return errors.NewError(errors.DbError, err.Error())
+	}
+	return nil
+}
+
+func getSession() *mgo.Session {
+	if mgoSession == nil {
+		var err error
+		mgoSession, err = mgo.Dial(MongoAddr)
+		//log.Println(MongoAddr)
+		if err != nil {
+			log.Println(err) // no, not really
+		}
+	}
+	return mgoSession.Clone()
+}
+
+func withCollection(collection string, safe *mgo.Safe, s func(*mgo.Collection) error) error {
+	session := getSession()
+	defer session.Close()
+
+	session.SetSafe(safe)
+	c := session.DB(databaseName).C(collection)
+	return s(c)
+}
+
+func SaveToDB(collection string, o interface{}, safe bool) error {
+	var err error
+	insert := func(c *mgo.Collection) error {
+		log.Println("c:", c)
+		log.Println("o :", o)
+		return c.Insert(o)
+	}
+
+	if safe {
+		err = withCollection(collection, &mgo.Safe{}, insert)
+	} else {
+		err = withCollection(collection, nil, insert)
+	}
+
+	if err != nil {
+		log.Println(err)
+		return errors.NewError(errors.DbError, err.(*mgo.LastError).Error())
+	}
+
+	return nil
+}
---
+++ pushmessage.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"github.com/garyburd/redigo/redis"
+	"github.com/shevilangle/pushserver/errors"
+	"github.com/shevilangle/pushserver/models"
+	"github.com/shevilangle/transfer"
+	"labix.org/v2/mgo/bson"
+	"log"
+	"time"
+)
+
+var (
+	redisServer string
+	fromString  string
+	toString    string
+	meterToLoc  float64
+	eventColl   string
+)
+
+type MsgBody struct {
+	Type    string `json:"type"`
+	Content string `json:"content"`
+}
+
+type EventData struct {
+	Type string    `json:"type"`
+	Id   string    `json:"pid"`
+	From string    `json:"from"`
+	To   string    `json:"to"`
+	Body []MsgBody `json:"body"`
+}
+
+type Event struct {
+	Id   bson.ObjectId `bson:"_id,omitempty" json:"-"`
+	Type string        `json:"type"`
+	Data EventData     `json:"push"`
+	Time int64         `json:"time"`
+}
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	flag.StringVar(&fromString, "f", "sports:pubsub:notice", "listen channel")
+	flag.StringVar(&toString, "t", "sports:pubsub:user:", "prefix of receiver channel")
+	flag.StringVar(&redisServer, "r", "172.24.222.54:6379", "redis server")
+	flag.StringVar(&models.MongoAddr, "m", "localhost:27017", "mongodb server")
+	flag.Parse()
+}
+
+func main() {
+	eventColl = "events"
+	c := float64(10000) / float64(111319)
+	meterToLoc = c
+	log.Println("fromString is :", fromString)
+	log.Println("toString is :", toString)
+	p := &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", redisServer)
+			if err != nil {
+				log.Println(err)
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+	t := transfer.NewTransfer(p, fromString, toString, getNearbyUsers, saveDataToDB)
+	t.Push()
+	log.Println("over")
+}
+
+func getNearbyUsers(data []byte) (error, []string, []interface{}) {
+	var event Event
+	err := json.Unmarshal(data, &event)
+	if err != nil {
+		log.Println(err)
+		return err, make([]string, 0), make([]interface{}, 0)
+	}
+
+	userid := event.Data.From
+	log.Println("userid: ", userid)
+
+	user := &models.Account{}
+	if find, err := user.FindByUserid(userid); !find {
+		if err == nil {
+			err = errors.NewError(errors.NotExistsError, "user '"+userid+"' not exists")
+		}
+		log.Println("not find")
+		return err, make([]string, 0), make([]interface{}, 0)
+	} else {
+		query := bson.M{
+			"loc": bson.M{
+				"$near":        []float64{user.Loc.Lat, user.Loc.Lng},
+				"$maxDistance": meterToLoc,
+			},
+			"_id": bson.M{
+				"$ne": userid,
+			},
+		}
+		_, u, e := models.GetListByQuery(query)
+		if e != nil {
+			log.Println("e :", e)
+			return e, make([]string, 0), make([]interface{}, 0)
+		}
+		usercount := len(u)
+		log.Println("usercount is :", usercount)
+
+		list := make([]string, usercount)
+		for i, v := range u {
+			list[i] = v.Id
+		}
+
+		events := make([]Event, usercount)
+		es := make([]interface{}, usercount)
+		for i, v := range u {
+			events[i].Id = bson.NewObjectId()
+			events[i].Type = event.Type
+			events[i].Time = event.Time
+			events[i].Data = event.Data
+			//			log.Println("events[i].Data.from:", events[i].Data.From)
+			events[i].Data.To = v.Id
+			es[i] = events[i]
+		}
+		return nil, list, es
+	}
+}
+
+func saveDataToDB(o interface{}) {
+	e := models.SaveToDB(eventColl, o, true)
+	if e != nil {
+		log.Println("e:", e)
+	}
+}
